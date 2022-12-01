5e3cf099686b40f30fdd96e14cdcd7d98a8fc6e8
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2022-12-01 15:00:04 +0700

    asdf

--- resources/views/gallery2.blade.php
+++ resources/views/gallery2.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class='allpictures'>
-       @foreach($data['galleries']['0'] as $gallery)
+       @foreach($data['galleries'][0] as $gallery)
            <div class='picture'>
-               <a href="{{asset('storage/ProjectImages/'.$gallery['0']->picture)}}" data-lightbox="example-set" data-title="{{$gallery['0']->ProjectTitle}}">
-                   <img src="{{asset('storage/ProjectImages/'.$gallery['0']->picture)}}" alt="" width="100%" height="100%"/>
+               <a href="{{asset('storage/ProjectImages/'.$gallery[0]->picture)}}" data-lightbox="example-set" data-title="{{$gallery[0]->ProjectTitle}}">
+                   <img src="{{asset('storage/ProjectImages/'.$gallery[0]->picture)}}" alt="" width="100%" height="100%"/>
                </a>
            </div>
         @endforeach
