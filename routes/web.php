f10799e5af647184a40fec35ee3e5664199197ec
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2022-12-01 14:40:39 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/gallery2', function () {
     // fetcj data from api
-    $response = Http::get('https://luthfi-ppw2.herokuapp.com/api/gallery')->json_decode();
-    return dd($response);
+    $response = Http::get('https://luthfi-ppw2.herokuapp.com/api/gallery');
+    $data = json_decode($response->false);
+    return dd($data);
     // return view('gallery2')->with($response);
 });
