1974a9bc308ffeb6db84da59c47922be3577c54d
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2022-12-01 14:53:56 +0700

    /

--- resources/views/gallery2.blade.php
+++ resources/views/gallery2.blade.php
@@ -8,5 +8,6 @@
                    <img src="{{asset('storage/ProjectImages/'.$gallery->picture)}}" alt="" width="100%" height="100%"/>
                </a>
            </div>
+        @endforeach
     </div>
 @endsection
